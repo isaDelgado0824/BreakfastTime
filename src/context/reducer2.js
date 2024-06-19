a9f7c5bbbd60bd860f3dd92b0195afc630690ea1
isaDelgado0824 <isabella.delgado824@gmail.com>
2024-06-19 18:38:11 -0500

    Fix

--- src/context/reducer2.js
+++ src/context/reducer2.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import types from './types';
 
 const reducer2 = (state = [], action) => {
--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from 'react'
 import { products } from '../data/data'
 import { Link } from 'react-router-dom'
 
